a4ec1b88b0b53fcc462f2cc3d117306de6d83587
simek <gosimek@gmail.com>
2020-08-19 18:13:56 +0200

    🚧 setIsSecondaryClickToShowDevMenuEnabled is not public yet

--- index.js
+++ index.js
@@ -6,4 +6,7 @@ import { AppRegistry } from 'react-native';
 import App from './src/App';
 import { name as appName } from './app.json';
 
+// TODO: https://github.com/microsoft/react-native-macos/pull/528
+// NativeModules.DevSettings.setIsSecondaryClickToShowDevMenuEnabled(true);
+
 AppRegistry.registerComponent(appName, () => App);
