3ca052826b6188d5738389fb27b64abbcf70dc0f
simek <gosimek@gmail.com>
2020-09-07 13:21:17 +0200

    ✨ add ability to disable developer menu in debug build 🔨 tweaks for the release build ⬆️ bump react-native-macos to latest release

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ import { AppRegistry } from 'react-native';
 import App from './src/App';
 import { name as appName } from './app.json';
 
-// TODO: https://github.com/microsoft/react-native-macos/pull/528
-// NativeModules.DevSettings.setIsSecondaryClickToShowDevMenuEnabled(true);
+NativeModules.DevSettings.setIsSecondaryClickToShowDevMenuEnabled(false);
 
 AppRegistry.registerComponent(appName, () => App);
