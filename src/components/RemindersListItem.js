e41f8eff37f19d15edbdeee55c7115b1b4a51e7b
Test User <gosimek@gmail.com>
2021-07-04 22:21:51 +0200

    💄 add outline to the focused RemindersListItem ⬆️ bump RN macOS to the latest version ⬆️ bump other dependencies

--- src/components/RemindersListItem.js
+++ src/components/RemindersListItem.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import type { Node } from 'react';
 import { ActionSheetIOS, Alert, StyleSheet, Text, TextInput, TouchableOpacity } from 'react-native';
 
@@ -12,94 +12,119 @@ const RemindersListItem: () => Node = ({
   onEdit,
   onEditEnd,
   onRename,
-}) => (
-  <TouchableOpacity
-    onPress={(e) => {
-      if (e.nativeEvent.button === 2) {
-        ActionSheetIOS.showActionSheetWithOptions(
-          {
-            options: ['Show Info', 'Rename', 'Delete'],
-          },
-          (buttonIndex) => {
-            if (buttonIndex === 0) {
-              Alert.prompt(
-                `"${item.title}" Info`,
-                undefined,
-                [
-                  {
-                    text: 'OK',
-                    onPress: (text) => {
-                      onEdit(text);
-                      onEditEnd(text);
+}) => {
+  const [focused, setFocused] = useState(false);
+  return (
+    <TouchableOpacity
+      onPress={(e) => {
+        if (e.nativeEvent.button === 2) {
+          ActionSheetIOS.showActionSheetWithOptions(
+            {
+              options: ['Show Info', 'Rename', 'Delete'],
+            },
+            (buttonIndex) => {
+              if (buttonIndex === 0) {
+                Alert.prompt(
+                  `"${item.title}" Info`,
+                  undefined,
+                  [
+                    {
+                      text: 'OK',
+                      onPress: (text) => {
+                        onEdit(text);
+                        onEditEnd(text);
+                      },
                     },
-                  },
-                  { text: 'Cancel' },
-                ],
-                undefined,
-                item.title,
-              );
-            } else if (buttonIndex === 1) {
-              onRename(e);
-            } else if (buttonIndex === 2) {
-              onLongPress(e);
+                    { text: 'Cancel' },
+                  ],
+                  undefined,
+                  item.title,
+                );
+              } else if (buttonIndex === 1) {
+                onRename(e);
+              } else if (buttonIndex === 2) {
+                onLongPress(e);
+              }
+            },
+          );
+        } else {
+          onPress(e);
+        }
+      }}
+      onLongPress={onLongPress}
+      onPressIn={(e) => {
+        if (e.nativeEvent.button === 2) {
+          setFocused(true);
+        }
+      }}
+      onPressOut={() => setFocused(false)}
+      style={[
+        styles.listItem,
+        item.selected ? styles.listItemSelected : {},
+        focused
+          ? {
+              borderColor: {
+                semantic: item.selected ? 'labelColor' : 'selectedContentBackgroundColor',
+              },
             }
-          },
-        );
-      } else {
-        onPress(e);
-      }
-    }}
-    onLongPress={onLongPress}
-    style={[styles.listItem, item.selected ? styles.listItemSelected : {}]}
-    activeOpacity={0.66}>
-    <RoundIcon icon="􀋲" iconSize={13} color={item.color} style={styles.listItemIcon} />
-    {item.editMode ? (
-      <TextInput
-        autoFocus={true}
-        value={item.title}
-        style={styles.listItemInput}
-        blurOnSubmit={true}
-        onSubmitEditing={(e) => {
-          onEditEnd(e.nativeEvent.text);
-        }}
-        onChangeText={onEdit}
-      />
-    ) : (
+          : {},
+      ]}
+      activeOpacity={1}>
+      <RoundIcon icon="􀋲" iconSize={13} color={item.color} style={styles.listItemIcon} />
+      {item.editMode ? (
+        <TextInput
+          autoFocus={true}
+          value={item.title}
+          style={styles.listItemInput}
+          blurOnSubmit={true}
+          onSubmitEditing={(e) => {
+            onEditEnd(e.nativeEvent.text);
+          }}
+          onChangeText={onEdit}
+        />
+      ) : (
+        <Text
+          numberOfLines={1}
+          ellipsizeMode="tail"
+          style={[
+            styles.listItemText,
+            {
+              color: item.selected ? '#fff' : { semantic: 'labelColor' },
+            },
+          ]}>
+          {item.title}
+        </Text>
+      )}
       <Text
-        numberOfLines={1}
-        ellipsizeMode="tail"
         style={[
-          styles.listItemText,
+          styles.listItemCounter,
           {
-            color: item.selected ? '#fff' : { semantic: 'labelColor' },
+            color: item.selected ? '#fff' : { semantic: 'secondaryLabelColor' },
           },
         ]}>
-        {item.title}
+        {count || 0}
       </Text>
-    )}
-    <Text
-      style={[
-        styles.listItemCounter,
-        {
-          color: item.selected ? '#fff' : { semantic: 'secondaryLabelColor' },
-        },
-      ]}>
-      {count || 0}
-    </Text>
-  </TouchableOpacity>
-);
+    </TouchableOpacity>
+  );
+};
 
 const styles = StyleSheet.create({
   listItem: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     paddingHorizontal: 14,
-    paddingVertical: 6,
+    paddingVertical: 4,
     borderRadius: 6,
+    borderWidth: 2,
+    borderStyle: 'solid',
+    borderColor: 'transparent',
     marginHorizontal: 10,
   },
   listItemSelected: {
     backgroundColor: {
+      semantic: 'selectedContentBackgroundColor',
+    },
+    borderColor: {
       semantic: 'selectedContentBackgroundColor',
     },
   },
